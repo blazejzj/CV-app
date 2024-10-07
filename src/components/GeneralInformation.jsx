b6e56622b888c720a78b9ab09eddf56e73fdeb8b
blazejzj <opexrrator@gmail.com>
2024-10-07 22:04:39 +0200

    refactor: DOMS split, added more functions + default form submission handled

--- src/components/GeneralInformation.jsx
+++ src/components/GeneralInformation.jsx
@@ -1,75 +1,78 @@
 import { useState } from "react";
 
-function renderEditMode(name, email, phoneNumber, setName, setEmail, setPhoneNumber, setIsEditing) {
-    
-    function handleSubmit(e) {
-        e.preventDefault(); // handle form submission page refresh
-        setIsEditing(false); // switch back to view mode
-    }
 
-    return (
-        <div>
-            <h1>General Information</h1>
-            <form onSubmit={handleSubmit}> {/* Attaches handleSubmit to form's onSubmit */}
+
+function GeneralInformation() {
+
+    function renderEditMode() {
+    
+        function handleSubmit(e) {
+            e.preventDefault(); // handle form submission page refresh
+            setIsEditing(false); // switch back to view mode
+        }
+    
+        return (
+            <div>
+                <h1>General Information</h1>
+                <form onSubmit={handleSubmit}> {/* Attaches handleSubmit to form's onSubmit */}
+                    <div>
+                        <label htmlFor="name">Name</label>
+                        <input 
+                            type="text"
+                            id="name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email">E-mail</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="phoneNumber">Phone Number</label>
+                        <input 
+                            type="text"
+                            id="phoneNumber"
+                            value={phoneNumber}
+                            onChange={(e) => setPhoneNumber(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit">
+                        Save
+                    </button>
+                </form>
+            </div>
+        );
+    }
+    
+    function renderViewMode() {
+        return (
+            <div>
+                <h1>General Information</h1>
                 <div>
                     <label htmlFor="name">Name</label>
-                    <input 
-                        type="text"
-                        id="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
+                    <p>{name}</p>
                 </div>
                 <div>
                     <label htmlFor="email">E-mail</label>
-                    <input 
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
+                    <p>{email}</p>
                 </div>
                 <div>
                     <label htmlFor="phoneNumber">Phone Number</label>
-                    <input 
-                        type="text"
-                        id="phoneNumber"
-                        value={phoneNumber}
-                        onChange={(e) => setPhoneNumber(e.target.value)}
-                    />
+                    <p>{phoneNumber}</p>
                 </div>
-                <button type="submit">
-                    Save
+                <button type="button" onClick={() => setIsEditing(true)}>
+                    Edit
                 </button>
-            </form>
-        </div>
-    );
-}
+            </div>
+        );
+    }
 
-function renderViewMode(name, email, phoneNumber, setIsEditing) {
-    return (
-        <div>
-            <h1>General Information</h1>
-            <div>
-                <label htmlFor="name">Name</label>
-                <p>{name}</p>
-            </div>
-            <div>
-                <label htmlFor="email">E-mail</label>
-                <p>{email}</p>
-            </div>
-            <div>
-                <label htmlFor="phoneNumber">Phone Number</label>
-                <p>{phoneNumber}</p>
-            </div>
-            <button type="button" onClick={() => setIsEditing(true)}>
-                Edit
-            </button>
-        </div>
-    );
-}
-
-function GeneralInformation() {
     const [name, setName] = useState("John Doe");
     const [email, setEmail] = useState("johndoe@react.com");
     const [phoneNumber, setPhoneNumber] = useState("+47 123 456 78");
@@ -78,8 +81,8 @@ function GeneralInformation() {
     return (
         <div>
             {isEditing 
-                ? renderEditMode(name, email, phoneNumber, setName, setEmail, setPhoneNumber, setIsEditing) 
-                : renderViewMode(name, email, phoneNumber, setIsEditing)
+                ? renderEditMode() 
+                : renderViewMode()
             }
         </div>
     );
